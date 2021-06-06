78738560663061b124cfc2085ee248f7f2247914
PanPeople <30329682+PanPeople@users.noreply.github.com>
2021-06-06 09:19:13 +0200

    added branch

---
+++ F103C8TX/PTM_project/Core/Inc/lcd_5110.h
@@ -0,0 +1,66 @@
+/*
+ * lcd_5110.h
+ *
+ *  Created on: May 30, 2021
+ *      Author: PanPeople
+ */
+
+#ifndef INC_LCD_5110_H_
+#define INC_LCD_5110_H_
+
+#define LCD_X 84
+#define LCD_Y 48
+
+#define LCD_C GPIO_PIN_RESET//<- command
+#define LCD_D GPIO_PIN_SET// <- data
+
+
+
+#define LCD_DC_PIN GPIO_PIN_1
+#define LCD_CE_PIN GPIO_PIN_6
+#define LCD_RST_PIN GPIO_PIN_0
+
+#define LCD_DC_PORT GPIOB
+#define LCD_CE_PORT GPIOA
+#define LCD_RST_PORT GPIOB
+
+
+
+
+
+
+void LCD_Initialize(SPI_HandleTypeDef *spi);
+void LCD_Command(SPI_HandleTypeDef *spi,uint8_t cmd);
+void LCD_Data(SPI_HandleTypeDef *spi,uint8_t* cmd);
+void LCD_Clear(SPI_HandleTypeDef *spi);
+void LCD_Fill(SPI_HandleTypeDef *spi);
+void LCD_draw_pixel(uint8_t buffer[],uint8_t x, uint8_t y);//to save cycles, after this it is required to use LCD_Refresh();
+void LCD_Draw_Line(SPI_HandleTypeDef *spi);
+void LCD_Printf(SPI_HandleTypeDef *spi);
+void LCD_Clear_Buffer(uint8_t buffer[]);
+void LCD_Data_Byte(SPI_HandleTypeDef *spi,uint8_t* cmd);
+
+void lcd_data(SPI_HandleTypeDef *spi,const uint8_t* data);
+void lcd_draw_text(int row, int col, const char* text,uint8_t buffer[]);
+
+extern const uint8_t logo[];
+/*
+ 7-17-2011
+ Spark Fun Electronics 2011
+ Nathan Seidle
+
+ This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+
+ This code writes a series of images and text to the Nokia 5110 84x48 graphic LCD:
+ http://www.sparkfun.com/products/10168
+
+ Do not drive the backlight with 5V. It will smoke. However, the backlight on the LCD seems to be
+ happy with direct drive from the 3.3V regulator.
+ Although the PCD8544 controller datasheet recommends 3.3V, the graphic Nokia 5110 LCD can run at 3.3V or 5V.
+ No resistors needed on the signal lines.
+
+ You will need 5 signal lines to connect to the LCD, 3.3 or 5V for power, 3.3V for LED backlight, and 1 for ground.
+ */
+extern const uint8_t font_ASCII[][5];
+
+#endif /* INC_LCD_5110_H_ */
---
+++ F103RB/PTM_project_master/Core/Src/main.c
@@ -0,0 +1,390 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "spi.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "lcd_5110.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint8_t buffer[LCD_X*LCD_Y/2];
+
+
+uint8_t flag [3] = {0,0,0};
+//flag:
+//flag[0] <- timer update
+//flag[1] <- buffer has been drawn
+//flag[2] <- direction "0" for outside/left "1" for inside/right animation
+
+
+
+//uint8_t dron [][]={
+//		{0,0},{1,0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,0},{8,0}
+//		     ,{1,1}			   ,{4,1}		     ,{7,1}
+//			 ,{1,2},{2,2},{3,2},{4,2},{5,2},{6,2},{7,2},
+//		{0,3}	   ,{2,3},{3,3},{4,3},{5,3},{6,3},{7,3},{8,3},
+//		{0,4}	   ,{2,4},{3,4},{4,4},{5,4},{6,4},{7,4},{8,4}
+//		 	 ,{1,5},{2,5},{3,5},{4,5},{5,5},{6,5},{7,5}
+//			 ,{1,6}			   ,{4,6}		     ,{7,6},
+//		{0,7},{1,7},{2,7},{3,7},{4,7},{5,7},{6,7},{7,7},{8,7}
+//
+//};
+uint8_t dron [27*2][2]={
+				{0,0},{1,0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,0},{8,0}
+				     ,{1,1}			   ,{4,1}		     ,{7,1}
+					 ,{1,2},{2,2},{3,2},{4,2},{5,2},{6,2},{7,2},
+				{0,3}	   ,{2,3},{3,3},{4,3},{5,3},{6,3},{7,3},{8,3},
+				{0,4}	   ,{2,4},{3,4},{4,4},{5,4},{6,4},{7,4},{8,4}
+				 	 ,{1,5},{2,5},{3,5},{4,5},{5,5},{6,5},{7,5}
+					 ,{1,6}			   ,{4,6}		     ,{7,6},
+				{0,7},{1,7},{2,7},{3,7},{4,7},{5,7},{6,7},{7,7},{8,7}
+};
+
+const uint8_t template [] = {
+0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x22, 0x22, 0x22, 0x22, 0x22, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x22, 0x22, 0x22, 0x22, 0x22, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x22, 0x22, 0x22,
+0x22, 0x22, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF,
+0xFF, 0x22, 0x22, 0x22, 0x22, 0x22, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x22, 0x22, 0x22, 0x22, 0x22, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x22, 0x22, 0x22, 0x22, 0x22, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+
+uint8_t depth = 20;
+uint16_t preasure = 2137;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_NVIC_Init(void);
+/* USER CODE BEGIN PFP */
+uint16_t map( uint16_t x, uint16_t in_min,uint16_t  in_max,uint16_t  out_min,uint16_t  out_max);
+
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+	LCD_Clear_Buffer(buffer);
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_SPI2_Init();
+  MX_SPI1_Init();
+  MX_TIM1_Init();
+
+  /* Initialize interrupts */
+  MX_NVIC_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_Base_Start_IT(&htim1);
+
+  LCD_Initialize(&hspi1);
+  //LCD_Clear();
+
+
+  for (uint16_t i = 0; i< LCD_X*LCD_Y/8 ; i++){
+	  buffer[i] = logo[i];
+  }
+
+  lcd_draw_text(0, 60, "v0.9", buffer);
+//  lcd_draw_text(1, 0, "Hej", buffer);
+//  char tmp[16];
+//  sprintf(tmp, "%d", 2137);
+//  lcd_draw_text(0, 60,tmp, buffer);
+
+  LCD_Data(&hspi1, buffer);
+  HAL_Delay(1000);
+
+  uint8_t r = 20;
+
+
+
+
+  for (uint16_t i = 0; i< LCD_X*LCD_Y/8 ; i++){
+	  buffer[i] = template[i];
+  }
+
+  //uint8_t
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+	  if(flag[1] == 0){
+
+
+		  flag[0] = 0;
+
+			for(uint16_t i = 0; i < (LCD_X*LCD_Y/8); i ++){
+				buffer[i]= 0x00;
+			}
+
+			  for (uint16_t i = 0; i< LCD_X*LCD_Y/8 ; i++){
+				  buffer[i] = template[i];
+			  }
+
+			for(uint8_t x = 0 ; x < LCD_X ; x++ ){
+			  LCD_draw_pixel(buffer,x, 0);
+			  LCD_draw_pixel(buffer,x, LCD_Y);
+			}
+
+
+			for(uint8_t y = 0 ; y < LCD_X ; y++ ){
+			  LCD_draw_pixel(buffer,0, y);
+			  LCD_draw_pixel(buffer,LCD_X, y);
+			}
+
+
+
+			for(uint8_t i = 0; i< 54 ; i ++){
+				LCD_draw_pixel(buffer,dron[i][0]+ 11 , dron[i][1]+r);
+			}
+
+			  lcd_draw_text(1, 25, "Preasure:", buffer);
+			  char tmp[16];
+			  sprintf(tmp, "%d hPa", preasure);
+			  lcd_draw_text(2, 25,tmp, buffer);
+			  lcd_draw_text(3, 25, "Depth:", buffer);
+			  sprintf(tmp, "%d m", depth);
+			  lcd_draw_text(4, 25,tmp, buffer);
+
+
+	  	  flag[1]=1;
+
+/*
+	  	  if(r > LCD_Y-8){
+	  		  flag[2] = 1;
+	  	  }
+	  	  else if(r == 0){
+	  		  flag[2] = 0;
+	  	  }
+
+	  	  if(flag[2] == 0){
+	  		r++;
+	  	  }
+	  	  else if (flag[2] == 1){
+	  		r--;
+	  	  }
+
+	  	  */
+
+	  }
+
+
+
+
+
+
+	  if(flag[0] == 1 && flag[1]== 1) {
+		  LCD_Data(&hspi1, buffer);
+		  flag[1] = 0;
+
+	  }
+
+
+
+	  //previous frame has been drawn.
+	  //1/30 has been passed and our next frame is ready
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief NVIC Configuration.
+  * @retval None
+  */
+static void MX_NVIC_Init(void)
+{
+  /* TIM1_UP_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(TIM1_UP_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(TIM1_UP_IRQn);
+}
+
+/* USER CODE BEGIN 4 */
+uint16_t map( uint16_t x, uint16_t in_min,uint16_t  in_max,uint16_t  out_min,uint16_t  out_max){
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+
+	if(htim->Instance == TIM1){
+
+		flag[0] = 1;
+	}
+
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
